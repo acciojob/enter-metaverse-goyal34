ce394248525af9acef372dc1b69acf54bf381ebf
goyal34 <119398857+goyal34@users.noreply.github.com>
2026-07-12 17:57:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+let status = document.getElementById("status");
+let enterBtn = document.getElementById("enterBtn");
+
+enterBtn.addEventListener("click", function () {
+    status.innerHTML = "<h1>Entered Metaverse</h1>";
+});
